70be23a986ba15130ab90c9c9cf69b468e7bfecd
Shikhar2004 <85475710+Shikhar2004@users.noreply.github.com>
2021-06-22 10:40:00 +0530

    Updates editor_gui.py

--- editor_gui.py
+++ editor_gui.py
@@ -23,51 +23,64 @@ window.tk_setPalette(background="#282828", foreground="#ebdbb2")
 Topic = tk.Label(window, text="Student Marksheet", fg = "#b8bb26", font = ("Bahnschrift",30))
 Topic.place(x=500,y=10)
 
-# Name field
-Nam_lbl = tk.Label(window, text="Student Name", font = ("Bahnschrift",15))
-Nam_lbl.place(x = 82, y = 105) 
+# Student Personal Info
+Data_lbl = tk.Label(window, text="Student Data:", font = ("Bahnschrift",20))
+Data_lbl.place(x = 60, y = 125) 
 
-Nam_txt = tk.Entry(window, bd = 2)
-Nam_txt.place(x = 60, y = 130)
+FirstNam_lbl = tk.Label(window, text = "First Name")
+FirstNam_lbl.place(x = 30, y = 200)
+FirstNam_txt = tk.Entry(window, bd = 2)
+FirstNam_txt.place(x = 120, y = 200)
+
+LastNam_lbl = tk.Label(window, text = "Last Name")
+LastNam_lbl.place(x=30 , y=250 )
+LastNam_txt = tk.Entry(window, bd = 2)
+LastNam_txt.place(x = 120, y = 250 )
+
+Roll_lbl = tk.Label(window, text = "Roll No.")
+Roll_lbl.place(x = 30, y = 300)
+Roll_txt = tk.Entry(window, bd =2)
+Roll_txt.place(x = 120, y = 300)
 
 # Header
-marks_lbl = tk.Label(window, text="Marks scored in:", font=("Bahnschrift",20))
+marks_lbl = tk.Label(window, text="Academic Data:", font=("Bahnschrift",20))
 marks_lbl.place(x=380, y=125)
 
 # Subject-wise
-Phy_lbl = tk.Label(window, text ="Physics")
-Phy_lbl.place(x=520,y=200)
+Phy_lbl = tk.Label(window, text ="Marks in Physics")
+Phy_lbl.place(x=350,y=200)
 
-Che_lbl = tk.Label(window, text ="Chemistry")
-Che_lbl.place(x=520,y=250)
+Che_lbl = tk.Label(window, text ="Marks in Chemistry")
+Che_lbl.place(x=350,y=250)
 
-Mat_lbl = tk.Label(window, text ="Maths")
-Mat_lbl.place(x=520,y=300)
+Mat_lbl = tk.Label(window, text ="Marks in Maths")
+Mat_lbl.place(x=350,y=300)
 
-Cs_lbl =  tk.Label(window, text ="CS")
-Cs_lbl.place(x=520,y=350)
+Cs_lbl =  tk.Label(window, text ="Marks in CS")
+Cs_lbl.place(x=350,y=350)
 
-Eng_lbl = tk.Label(window, text ="English")
-Eng_lbl.place(x=520,y=400)
+Eng_lbl = tk.Label(window, text ="Marks in English")
+Eng_lbl.place(x=350,y=400)
 
 
 # Inputs for each subject
 Phy_txt = tk.Entry(window, bd = 2)
-Phy_txt.place(x=610, y = 200)
+Phy_txt.place(x=480, y = 200)
 
 Che_txt = tk.Entry(window, bd = 2)
-Che_txt.place(x=610, y = 250)
+Che_txt.place(x=480, y = 250)
 
 Mat_txt = tk.Entry(window, bd = 2)
-Mat_txt.place(x=610, y = 300)
+Mat_txt.place(x=480, y = 300)
 
 Cs_txt = tk.Entry(window, bd = 2)
-Cs_txt.place(x=610, y = 350)
+Cs_txt.place(x=480, y = 350)
 
 Eng_txt = tk.Entry(window, bd = 2)
-Eng_txt.place(x=610, y = 400)
+Eng_txt.place(x=480, y = 400)
 
 
 window.mainloop()
+
 
 
