d3c259a3e45d35a4397d0b10ccc3609b948ed474
Shikhar2004 <85475710+Shikhar2004@users.noreply.github.com>
2021-06-23 19:52:11 +0530

    Updates editor_gui.py

--- editor_gui.py
+++ editor_gui.py
@@ -42,51 +42,65 @@ LastNam_txt.place(x = 120, y = 250 )
 Roll_lbl = tk.Label(window, text = "Roll No.")
 Roll_lbl.place(x = 30, y = 300)
 Roll_txt = tk.Entry(window, bd =2)
-Roll_txt.place(x = 120, y = 300)
+Roll_txt.place(x = 98, y = 300)
+
+Adm_lbl = tk.Label(window, text = "Admission No.")
+Adm_lbl.place(x = 30, y = 350)
+Adm_txt = tk.Entry(window, bd = 2)
+Adm_txt.place(x = 136, y = 350)
+
+#RadioButton
+var = StringVar()
+var.set(' ')
+
+Gender_lbl = tk.Label(window, text = "Gender")
+Gender_lbl.place(x = 30, y = 510)
+Male = Radiobutton(window, text ="Male",variable = var, value = 1, selectcolor= "black")
+Male.place(x = 90, y = 500)
+
+Female = Radiobutton(window, text = "Female",variable = var, value = 2, selectcolor = "black")
+Female.place(x = 90, y = 520)
 
 # Header
 marks_lbl = tk.Label(window, text="Academic Data:", font=("Bahnschrift",20))
 marks_lbl.place(x=380, y=125)
 
-# Subject-wise
-Phy_lbl = tk.Label(window, text ="Marks in Physics")
-Phy_lbl.place(x=350,y=200)
+# Subject-wise CheckButtons
 
-Che_lbl = tk.Label(window, text ="Marks in Chemistry")
-Che_lbl.place(x=350,y=250)
+Selectedsub_frame = LabelFrame(window, text = "Subjects studying", font = ("Times New Roman", 17))
+Selectedsub_frame.place(x = 350, y = 200,width= 250, height = 240)
 
-Mat_lbl = tk.Label(window, text ="Marks in Maths")
-Mat_lbl.place(x=350,y=300)
+Phy, Che, Mat, Bio, His, Civ, Geo, Eco, Hin, Eng = IntVar(),IntVar(),IntVar(),IntVar(),IntVar(), \
+IntVar(),IntVar(),IntVar(),IntVar(),IntVar()
 
-Cs_lbl =  tk.Label(window, text ="Marks in CS")
-Cs_lbl.place(x=350,y=350)
+Phy_Check = Checkbutton(Selectedsub_frame, text = "Physics", variable=Phy, onvalue=1, offvalue=0, selectcolor="black")
+Phy_Check.place(x = 2, y= 0)
+Che_Check = Checkbutton(Selectedsub_frame, text = "Chemistry", variable=Che, onvalue=1, offvalue=0, selectcolor="black")
+Che_Check.place(x = 2, y= 40)
+Mat_Check = Checkbutton(Selectedsub_frame, text = "Maths", variable=Mat, onvalue=1, offvalue=0, selectcolor="black")
+Mat_Check.place(x = 2, y= 80)
+Bio_Check = Checkbutton(Selectedsub_frame, text = "Biology", variable=Bio, onvalue=1, offvalue=0, selectcolor="black")
+Bio_Check.place(x = 2, y= 120)
+His_Check = Checkbutton(Selectedsub_frame, text = "History", variable=His, onvalue=1, offvalue=0, selectcolor="black")
+His_Check.place(x = 2, y= 160)
+Civ_Check = Checkbutton(Selectedsub_frame, text = "Civics", variable=Civ, onvalue=1, offvalue=0, selectcolor="black")
+Civ_Check.place(x = 130, y= 0)
+Geo_Check = Checkbutton(Selectedsub_frame, text = "Geography", variable=Geo, onvalue=1, offvalue=0, selectcolor="black")
+Geo_Check.place(x = 130, y= 40)
+Eco_Check = Checkbutton(Selectedsub_frame, text = "Economics", variable=Eco, onvalue=1, offvalue=0, selectcolor="black")
+Eco_Check.place(x = 130, y= 80)
+Hin_Check = Checkbutton(Selectedsub_frame, text = "Hindi", variable=Hin, onvalue=1, offvalue=0, selectcolor="black")
+Hin_Check.place(x = 130, y= 120)
+Eng_Check = Checkbutton(Selectedsub_frame, text = "English", variable=Eng, onvalue=1, offvalue=0, selectcolor="black")
+Eng_Check.place(x = 130, y= 160)
 
-Eng_lbl = tk.Label(window, text ="Marks in English")
-Eng_lbl.place(x=350,y=400)
-
-
-# Inputs for each subject
-Phy_txt = tk.Entry(window, bd = 2)
-Phy_txt.place(x=480, y = 200)
-
-Che_txt = tk.Entry(window, bd = 2)
-Che_txt.place(x=480, y = 250)
-
-Mat_txt = tk.Entry(window, bd = 2)
-Mat_txt.place(x=480, y = 300)
-
-Cs_txt = tk.Entry(window, bd = 2)
-Cs_txt.place(x=480, y = 350)
-
-Eng_txt = tk.Entry(window, bd = 2)
-Eng_txt.place(x=480, y = 400)
 
 #Drop-Down Menu
 
 Standard = Label(window, text = "Standard", font = ("Times New Roman", 13))
-Standard.place(x = 30, y = 380)
+Standard.place(x = 20, y = 420)
 Section = Label(window, text = "Section", font = ("Times New Roman", 13))
-Section.place(x = 30, y = 430)
+Section.place(x = 150, y = 420)
 
 options1 = ["I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII"]
 options2 = ['A','B','C','D','E','F','G','H','I','J','K']
@@ -98,12 +112,12 @@ clicked2 = StringVar()
 clicked2.set("")
 
 drop1 = OptionMenu(window, clicked1, *options1)
-drop1.place(x = 100, y = 380)
+drop1.place(x = 86, y =420)
 
 drop2 = OptionMenu(window, clicked2, *options2)
-drop2.place(x = 100, y= 430)
+drop2.place(x = 210, y= 420)
+
 
 window.mainloop()
-
 
 
